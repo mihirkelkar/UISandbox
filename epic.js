f12dddd79e245a442d5e7228e05fbd0dd8d3308a
mihirkelkar <kelkarmhr@gmail.com>
2015-12-02 20:47:36 -0600

    Backing up just in case

---
+++ epic.js
@@ -0,0 +1,80 @@
+angular.module('epic', [])
+  .controller('searchCtrl', function($scope, $http){
+    $http({method: 'GET', url: '../data/search.json',
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
+            }}).success(function(data){
+          $scope.items = data;
+          console.log($scope.items);
+       });
+  })
+  .controller('HomeCtrl', function($scope, $http){
+    $scope.title = "Next Ready Patient";
+    $scope.name = "Schrute, Dwight";
+    $scope.pulse = "72";
+    $scope.bloodp = "123 / 89";
+    $scope.rov = "Pain in Left Ankle";
+    $scope.color = "#8EE336";
+    $scope.image = "http://www.eonline.com/eol_images/Entire_Site/20080508/300.willson.office.050808.jpg";
+    $http({method: 'GET', url: 'data/patients.json',
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
+            }}).success(function(data){
+          $scope.patients = data;
+          //console.log($scope.patients);
+    });
+    $scope.hoverIn = function(index){
+      $scope.title = $scope.patients[index].title;
+      $scope.image = $scope.patients[index].image;
+      $scope.bloodp = $scope.patients[index].bloodp;
+      $scope.pulse = $scope.patients[index].pulse;
+      $scope.rov = $scope.patients[index].rov;
+      $scope.name = $scope.patients[index].name;
+      $scope.color = $scope.patients[index].color;
+    };
+    $scope.hoverOut = function(){
+      $scope.title = "Next Ready Patient";
+      $scope.name = "Schrute, Dwight";
+      $scope.pulse = "72";
+      $scope.bloodp = "123 / 89";
+      $scope.rov = "Pain in Left Ankle";
+      $scope.color = "#8EE336";
+      $scope.image = "http://www.eonline.com/eol_images/Entire_Site/20080508/300.willson.office.050808.jpg";
+    }
+
+
+  })
+  .filter('searchFor', function(){
+	// All filters must return a function. The first parameter
+	// is the data that is to be filtered, and the second is an
+	// argument that may be passed with a colon (searchFor:searchString)
+	return function(arr, searchString){
+		if(!searchString){
+			return [];
+		}
+		var result = [];
+		searchString = searchString.toLowerCase();
+		// Using the forEach helper method to loop through the array
+		angular.forEach(arr, function(item){
+			if(item.title.toLowerCase().indexOf(searchString) !== -1){
+				result.push(item);
+			}
+    console.log("Found something");
+		});
+
+		return result;
+    };
+  })
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {
+      templateUrl : 'home.html',
+      controller : 'HomeCtrl'
+    }).when('/home', {
+      templateUrl :'login.html',
+      controller : ''
+    });
+  }])
