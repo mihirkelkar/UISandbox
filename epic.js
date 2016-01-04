c380067556bfb436bdc9d3977cf19681673dcd56
mihirkelkar <kelkarmhr@gmail.com>
2016-01-03 21:58:17 -0600

    Adding stuff

--- epic.js
+++ epic.js
@@ -51,6 +51,40 @@ angular.module('epic', [])
       $scope.age = "32";
       $scope.image = "http://www.eonline.com/eol_images/Entire_Site/20080508/300.willson.office.050808.jpg";
     };
+    $scope.patmsg = {
+      'name' : 'Sean Kravitz',
+      'sexage' : 'Male, 33 year old',
+      'dx' : 'Sinusitis',
+      'medication' :'No Medications',
+      'msg' : 'Sean called about a rash on his left leg. He is going on a trip this weekend.',
+      'image' : 'http://www.oliveoilsource.com/sites/default/files/stories/Tom-Mueller.a_0.jpg'
+    };
+    $scope.patmsgs = [
+      {
+        'name' : 'Ron Libowitz',
+        'sexage' : 'Male, 47 year old',
+        'dx' : 'GERD',
+        'medication' :'Zegetin',
+        'msg' : "Ron called saying his acid reflux isn't getting better. He is asking to see you again",
+        'image' : 'http://4.bp.blogspot.com/-a8ndvOIJn0M/UVI5Edq3-aI/AAAAAAACru8/LEeetksUqiU/s1600/Chris+Passport.jpg'
+      },
+      {
+        'name' : 'Steph Tull',
+        'sexage' : 'Female, 51 year old',
+        'dx' : 'Diabetes',
+        'medication' :'Insulin Injections',
+        'msg' : 'Asking if she could switch to a new cheaper injection',
+        'image' : 'http://consciouswealthinstitute.com/wp-content/uploads/2015/04/Justine-Musk.jpg'
+      },
+      {
+        'name' : 'Marc Anderson',
+        'sexage' : 'Male, 35 year old',
+        'dx' : 'Eye Infection',
+        'medication' :'Eyetone',
+        'msg' : 'Wants to know if the infection is contagious?',
+        'image' : 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/1/000/034/180/2fca843.jpg'
+      }
+    ]
     $http({method: 'GET', url: 'data/medrefill.json',
             headers:{
                 'Access-Control-Allow-Origin': '*',
@@ -63,6 +97,9 @@ angular.module('epic', [])
     $scope.reject = function(){
       console.log("Medication has been rejected");
       $scope.refills.pop();
+    };
+    $scope.newpatmsg = function(){
+      $scope.patmsg = $scope.patmsgs[Math.floor(Math.random() * (2 + 1)) + 0];
     }
   })
   .filter('searchFor', function(){
